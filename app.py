e8891791d883ea61bd519b00e8f7a7c84c6c7e9e
200505429 <99303945+200505429@users.noreply.github.com>
2022-07-20 02:54:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import  Flask, render_template,request
 from backEnd import backEnd
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+
 import json
 
 app= Flask(__name__)
